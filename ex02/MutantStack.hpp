b05c9125dcc0256ddd25018d6474cdceae07084a
Jordan Bax <jbax@f1r5s1.codam.nl>
2024-08-01 18:46:16 +0200

    fix inherit

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,51 +1,107 @@
 #pragma once
 #include <iostream>
 #include <stack>
+#include <iterator>
 
 template<typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
 private:
-	std::stack<T> _stack;
-	unsigned int 	_size;
-	// T*				
+	T*				_begin; // a pointer to the first element
 
 public:
-	MutantStack(){};
+	MutantStack():_begin(0){};
 	// MutantStack(  );
-	MutantStack( MutantStack const & src ){_stack = src._stack;};
+	MutantStack( MutantStack const & src ){*this = src;};
 	~MutantStack(){};
 
 	 class iterator : public std::iterator<
                                 std::input_iterator_tag, // iterator_category
-                                T,                    // value_type
-                                T,                    // difference_type
-                                const T*,             // pointer
-                                T                     // reference
+                                T*                    // value_type
                             > {
-        T num = FROM;
+        T* _pointer;
     public:
-        explicit iterator(T _num = 0) : num(_num) {}
-        iterator& operator++() { num = TO >= FROM ? num + 1: num - 1; return *this; }
-        iterator operator++(int) { iterator retval = *this; ++(*this); return retval; }
-        bool operator==(iterator other) const { return num == other.num; }
+        explicit iterator(T* pointer = 0) : _pointer(pointer) {}
+        iterator& operator--() { _pointer = _pointer - 1; return *this; }
+        iterator operator--(int) { iterator retvalue = *this; --(*this); return retvalue; }
+        iterator& operator++() { _pointer = _pointer + 1; return *this; }
+        iterator operator++(int) { iterator retvalue = *this; ++(*this); return retvalue; }
+        bool operator==(iterator other) const { return _pointer == other._pointer; }
         bool operator!=(iterator other) const { return !(*this == other); }
-        reference operator*() const { return num; }
+        T& operator*() const { return *_pointer; }
     };
-    iterator begin() { return iterator(FROM); }
-    iterator end() { return iterator(TO >= FROM? TO + 1 : TO - 1); }
+    iterator begin() { return iterator(_begin); }
+    iterator end() { return iterator(_begin + std::stack<T>::size()); }
+    iterator const cbegin()const { return iterator(_begin); }
+    iterator const cend()const { return iterator(_begin + std::stack<T>::size()); }
 
-	// MutantStack &	operator=( MutantStack const & rhs );
-	bool			empty()const{ return _stack.empty();};
-	unsigned int	size()const{ return _stack.size();};
-	T &				top(){return _stack.top();};
-	void			push(T content){_stack.push(content);};
-	void			pop(){_stack.pop();};
-	void			emplace(T content){_stack.emplace(content);};
-	void			swap(MutantStack<T> & rhs){_stack.swap(rhs);};
-	// static T*		iterator(){return };
-
-
+	MutantStack &	operator=( MutantStack const & rhs ){// need to manualy deep copy for accurate begin pointer
+		while (!std::stack<T>::empty()) // emptying the stack
+			std::stack<T>::pop();
+		MutantStack<int>::iterator it = rhs.cbegin();
+		MutantStack<int>::iterator ite = rhs.cend();
+		while (it != ite) // iterate for deep copy
+		{
+			this->push(*it);
+			it++;
+		}
+		return *this;
+	};
+	T &		operator[](unsigned int index) const { // extra Array subscript operator
+		if (index >= 0 && index < std::stack<T>::size()) 
+			return *(_begin + index);
+		else
+			throw std::bad_exception(); 
+	};
+	void			push(T content){std::stack<T>::push(content); if (std::stack<T>::size() == 1) _begin = &std::stack<T>::top();};
+	void			push_back(T content){std::stack<T>::push(content); if (std::stack<T>::size() == 1) _begin = &std::stack<T>::top();};
 };
+// old draft  is not correct
 
-// std::ostream & operator<<( std::ostream & o, MutantStack const & rhs){};
+// class MutantStack
+// {
+// private:
+// 	std::stack<T> _stack;
+// 	T*				_begin;
+
+// public:
+// 	MutantStack():_begin(0){};
+// 	// MutantStack(  );
+// 	MutantStack( MutantStack const & src ){_stack = src._stack;};
+// 	~MutantStack(){};
+
+// 	 class iterator : public std::iterator<
+//                                 std::input_iterator_tag, // iterator_category
+//                                 T*                    // value_type
+//                             > {
+//         T* _pointer;
+//     public:
+//         explicit iterator(T* pointer = 0) : _pointer(pointer) {}
+//         iterator& operator--() { _pointer = _pointer - 1; return *this; }
+//         iterator operator--(int) { iterator retvalue = *this; --(*this); return retvalue; }
+//         iterator& operator++() { _pointer = _pointer + 1; return *this; }
+//         iterator operator++(int) { iterator retvalue = *this; ++(*this); return retvalue; }
+//         bool operator==(iterator other) const { return _pointer == other._pointer; }
+//         bool operator!=(iterator other) const { return !(*this == other); }
+//         T& operator*() const { return *_pointer; }
+//     };
+//     iterator begin() { return iterator(_begin); }
+//     iterator end() { return iterator(_begin + _stack.size()); }
+
+// 	MutantStack &	operator=( MutantStack const & rhs ){_stack};
+// 	T &		operator[](unsigned int index) const {
+// 		if (index >= 0 && index < _stack.size()) 
+// 			return *(_begin + index);
+// 		else
+// 			throw std::bad_exception(); 
+// 	};
+// 	bool			empty()const{ return _stack.empty();};
+// 	unsigned int	size()const{ return _stack.size();};
+// 	T &				top(){return _stack.top();};
+// 	void			push(T content){_stack.push(content); if (_stack.size() == 1) _begin = &_stack.top();};
+// 	void			push_back(T content){_stack.push(content); if (_stack.size() == 1) _begin = &_stack.top();};
+// 	void			pop(){_stack.pop();};
+// 	void			pop_back(){_stack.pop();};
+// 	void			emplace(T content){_stack.emplace(content);};
+// 	void			swap(MutantStack<T> & rhs){_stack.swap(rhs);};
+// };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
 #include "MutantStack.hpp"
+#include <vector>
 
 int main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-//[...]
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+	// making and filling stacks
+	std::stack<int> test_stack;
+	MutantStack<int> mstack;
+	test_stack.push(5);
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << test_stack.top() << std::endl;
+	std::cout << mstack.top() << " " <<mstack[1] << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	// making en testing the iterators
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	MutantStack<int> mstack2(mstack);
+	mstack.pop();
+	it = mstack2.begin();
+	ite = mstack2.end();
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	// testing assignments of old stack
+	std::stack<int> s(mstack);
+	std::cout << std::endl;
+	while (!s.empty())
+	{
+		std::cout << s.top() << std::endl;
+		s.pop();
+	}
+	return 0;
 }
